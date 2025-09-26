72cd338e150fc9d6eaf70dfd9cdcac699a062be2
lifeofpaks <chukwudi.promise@yahoo.com>
2025-09-26 10:22:26 +0100

    updated headre

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,31 @@
 import { useState } from "react";
-import { FaUserCircle, FaSignOutAlt } from "react-icons/fa"; // user + logout icons
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import { FaSignOutAlt } from "react-icons/fa";
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
 
-  const handleToggle = () => setIsOpen((prev) => !prev);
+  // Example user info
+  const firstName = "Patrick";
+  const lastName = "Erwing";
+  const initials = `${firstName[0]}${lastName[0]}`;
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const handleLogout = () => {
     console.log("User logged out");
     // TODO: add your logout logic here
-    setIsOpen(false);
+    handleClose();
   };
 
   return (
@@ -23,96 +40,41 @@ const Header = () => {
             Micro Agents
           </a>
 
-          {/* Toggler */}
-          {/* <button
-            className="navbar-toggler offcanvas-nav-btn"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasNav"
-            aria-controls="offcanvasNav"
-          >
-            <i className="bi bi-list"></i>
-          </button> */}
-
-          {/* Offcanvas */}
-          {/* <div
-            className="offcanvas offcanvas-start offcanvas-nav"
-            tabIndex="-1"
-            id="offcanvasNav"
-            aria-labelledby="offcanvasNavLabel"
-            style={{ width: "20rem" }}
-          >
-            <div className="offcanvas-header">
-              <a
-                className="navbar-brand font-extrabold !text-[20px]"
-                href="./index.html"
-              >
-                MicroAgent
-              </a>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="offcanvas"
-                aria-label="Close"
-              ></button>
-            </div>
-
-            <div className="offcanvas-body pt-0 align-items-center">
-              <ul className="navbar-nav mx-auto align-items-lg-center">
-                <li>
-                  <a className="dropdown-item" href="./index.html">
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="./#product">
-                    Our AI Products
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="./#work">
-                    How it Works
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="./#pricing">
-                    Pricing
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="./#pricing">
-                    Contact Us
-                  </a>
-                </li>
-              </ul>
-
-              <div className="mt-3 mt-lg-0 d-flex align-items-center">
-                <a href="./#pricing" className="btn btn-primary">
-                  Get Started
-                </a>
-              </div>
-            </div>
-          </div> */}
-
           {/* User Dropdown */}
-          <div className="ms-auto position-relative">
-            <button
-              onClick={handleToggle}
-              className="btn btn-link text-dark p-0 border-0"
+          <div className="ms-auto">
+            <Avatar
+              sx={{
+                bgcolor: "#1e293b",
+                cursor: "pointer",
+                fontWeight: "bold",
+              }}
+              onClick={handleClick}
             >
-              <FaUserCircle size={28} />
-            </button>
-
-            {isOpen && (
-              <div className="dropdown-menu dropdown-menu-end show mt-2 shadow-lg rounded-lg border-0">
-                <button
-                  onClick={handleLogout}
-                  className="dropdown-item d-flex align-items-center gap-2 text-danger fw-medium"
-                >
-                  <FaSignOutAlt /> Logout
-                </button>
-              </div>
-            )}
+              {initials}
+            </Avatar>
+            <Menu
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+              transformOrigin={{ vertical: "top", horizontal: "right" }}
+              PaperProps={{
+                sx: {
+                  bgcolor: "#1e293b",
+                  color: "white",
+                  mt: 1,
+                  borderRadius: 2,
+                  minWidth: 150,
+                },
+              }}
+            >
+              <MenuItem onClick={handleLogout}>
+                <ListItemIcon sx={{ color: "white", minWidth: "32px" }}>
+                  <FaSignOutAlt size={16} />
+                </ListItemIcon>
+                Logout
+              </MenuItem>
+            </Menu>
           </div>
         </div>
       </nav>
