acc6cb332452b3b7b1c44d42e728b25c4c1ea4ce
lifeofpaks <chukwudi.promise@yahoo.com>
2025-09-26 12:57:00 +0100

    clean up code

--- src/components/ProductsSection.jsx
+++ src/components/ProductsSection.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 const ProductCard = ({ icon, title, status, description, delay }) => (
   <div className="col-lg-4 col-md-6 col-12">
-    <a href="#!">
+    <a href="">
       <div className="card border-gradient bg-[#1e293b]">
         <div className="card-body d-flex flex-column gap-6 p-5">
           <div className={`${icon} icon-shape icon-xl rounded-3`}>
